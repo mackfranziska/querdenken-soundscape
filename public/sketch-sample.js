66bdd9ba78fe22be5ae3685b67f3dc6e30e0d9b3
Franziska Mack <53446161+mackfranziska@users.noreply.github.com>
2021-07-14 10:53:07 -0400

    .gitignore is now working

--- public/sketch-sample.js
+++
@@ -1,7 +0,0 @@
-let socket = io();
-      let el;
-
-      socket.on('time', (timeString) => {
-        el = document.getElementById('server-time');
-        el.innerHTML = 'Server time: ' + timeString;
-      });
